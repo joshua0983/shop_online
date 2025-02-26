19bffd7143bef66113a31d56dbff18988973fa39
Joshua Jateno <joshuajateno@gmail.com>
2025-02-26 14:34:51 -0500

    refactor: restructure frontend application with new App, Navbar, and Footer components

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main>
+        <h1>Shop Online</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
