ef6b6b1708515445e2c3735aa5e872da5cd4cf19
Joshua Jateno <joshuajateno@gmail.com>
2025-06-07 03:15:28 -0400

    Started "body" of website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar.js';
+import Body from './Body.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Body />
     </div>
   );
 }
---
+++ frontend/src/Body.js
@@ -0,0 +1,11 @@
+import "./Body.css";
+
+function Body() {
+    return (
+        <div classname="body-container">
+            <h1>Trending</h1>
+        </div>
+
+    )
+}
+export default Body;
