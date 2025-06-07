6aced94a7d06471d4a2a826f0d813678d8d2a9c9
Joshua Jateno <joshuajateno@gmail.com>
2025-06-07 03:10:59 -0400

    added search

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -10,15 +10,27 @@ function Navbar() {
             </i>
             </a>
             <ul className="nav-menu">
-            <li className="nav-item">
-                <a href="/" className="nav-links"><i>Home</i></a>
-            </li>
-            <li className="nav-item">
-                <a href="/about" className="nav-links"><i>About</i></a>
-            </li>
-            <li className="nav-item">
-                <a href="/contact" className="nav-links"><i>Contact</i></a>
-            </li>
+                <li className="nav-item">
+                    <form className="nav-search">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="nav-search-input"
+                        />
+                        <button type="submit" className="nav-search-btn">
+                            <i>Go</i>
+                        </button>
+                    </form>
+                </li>
+                <li className="nav-item">
+                    <a href="/" className="nav-links"><i>Home</i></a>
+                </li>
+                <li className="nav-item">
+                    <a href="/about" className="nav-links"><i>About</i></a>
+                </li>
+                <li className="nav-item">
+                    <a href="/contact" className="nav-links"><i>Contact</i></a>
+                </li>
             </ul>
         </div>
         </nav>
