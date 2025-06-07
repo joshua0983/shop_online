95fbe1322e3d713a0e47036d89c56b3b5e03e7e2
Joshua Jateno <joshuajateno@gmail.com>
2025-06-07 03:08:55 -0400

    font changes

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -5,17 +5,19 @@ function Navbar() {
         <nav className="navbar">
         <div className="navbar-container">
             <a href="/" className="navbar-logo">
+            <i>
             SHOP ONLINE
+            </i>
             </a>
             <ul className="nav-menu">
             <li className="nav-item">
-                <a href="/" className="nav-links">Home</a>
+                <a href="/" className="nav-links"><i>Home</i></a>
             </li>
             <li className="nav-item">
-                <a href="/about" className="nav-links">About</a>
+                <a href="/about" className="nav-links"><i>About</i></a>
             </li>
             <li className="nav-item">
-                <a href="/contact" className="nav-links">Contact</a>
+                <a href="/contact" className="nav-links"><i>Contact</i></a>
             </li>
             </ul>
         </div>
