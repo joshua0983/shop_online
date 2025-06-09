1922ef230330e3d1d4c4b637c6fd2ab08a62cdff
Joshua Jateno <joshuajateno@gmail.com>
2025-06-08 21:57:02 -0400

    Connecting search with the backend

--- frontend/src/Body.js
+++ frontend/src/Body.js
@@ -1,9 +1,26 @@
+import React, {use, useEffect, useState} from "react";
 import "./Body.css";
 
 function Body() {
+    const [results, setResults] = useState([]);
+    const [searchQuery, setSearchQuery] = useState(false);
+    useEffect(() => {
+        if (!searchQuery) return;
+        setLoading(true);
+        fetch(`/search?query=${searchQuery}`)
+        .then(res => res.json())
+        .then(data => {
+            setResults(data.products || []);
+            setLoading(false);
+        });
+    }, [searchQuery]);
     return (
-        <div classname="body-container">
-            <h1>Trending</h1>
+        <div className="body-container">
+           { searchQuery ? (
+            <h2>Results for {searchQuery}</h2>
+              ) : (
+            <h2>No search query provided.</h2>
+           )}
         </div>
 
     )
